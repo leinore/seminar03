3b977a13e46a01da1dcc6ad8fa59781965204708
leinore <116540768+leinore@users.noreply.github.com>
2022-11-22 22:12:57 +0300

    add task_21э git commit -madd

---
+++ task_21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координату Х точки A:");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки A:");
+int Ay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Х точки B:");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки B:");
+int By = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2)), 2);
+Console.WriteLine(distance);
